32b3e2daba53b1a304ff0a47dec5cf89e29dc657
Student User <student@workstation.lab.example.com>
2023-05-06 02:25:14 -0400

    this is good exercise

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "This is a good learning exercise" "\n";
+print "This is a good learning exercise on build creation using source to image approach" "\n";
 ?>
